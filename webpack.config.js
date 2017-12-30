3d01f7b45f7d7bea2fa17af253a689842a99db0e
albertlockett <albert.lockett@gmail.com>
2017-12-30 09:58:52 -0400

    added other webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,32 @@
+const rules = [
+  {
+    test: /\.scss$/,
+    use: [
+      { loader: "style-loader" }, 
+      { loader: "css-loader" }, 
+      { loader: "sass-loader" }
+    ]
+  },
+  {
+    test: /\.css$/,
+    use: [
+      { loader: "style-loader" }, 
+      { loader: "css-loader" }
+    ]
+  }
+];
+
+
 export default {
   output: {
     filename: "index.js"
   },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: "style-loader" }, 
-          { loader: "css-loader" }, 
-          { loader: "sass-loader" }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: "style-loader" }, 
-          { loader: "css-loader" }
-        ]
-      }
-    ]
+  module: { rules }
 }
+
+export const webmail = {
+  output: {
+    filename: 'webmail.js'
+  },
+  module: { rules }
 }
