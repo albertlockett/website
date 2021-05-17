fea617c41509ad17fe6fcf8c321b9270dcea5d24
albertlockett <albert.lockett@gmail.com>
2021-05-17 18:51:31 -0400

    fixed css for links and mobile nav

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react'
+import classnames from 'classnames'
 import { Link } from 'react-router-dom'
 
 import './styles.scss'
@@ -6,13 +7,13 @@ import './styles.scss'
 export default function Navigation(): ReactElement {
   return (
     <div className="navigation">
-      <div className="link">
+      <div className={classnames('link', 'underline-fadein')}>
         <Link to="/">home</Link>
       </div>
-      <div className="link">
+      <div className={classnames('link', 'underline-fadein')}>
         <Link to="/cv">cv</Link>
       </div>
-      <div className="link">
+      <div className={classnames('link', 'underline-fadein')}>
         <Link to="/contact">contact</Link>
       </div>
     </div>
