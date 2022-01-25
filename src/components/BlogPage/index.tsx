b3d059406df1405ea0007da24679530c9f794b2a
albertlockett <albert.lockett@sonraisecurity.com>
2022-01-24 20:15:43 -0500

    poop

--- src/components/BlogPage/index.tsx
+++ src/components/BlogPage/index.tsx
@@ -1,14 +1,45 @@
 import React, { ReactElement } from 'react'
 import classnames from 'classnames'
+import { useLocation, Link, Switch, Route } from 'react-router-dom'
 
-import Helloo from './Test.mdx'
+import IneffJavaHashSet from './innefective-java/set-contains.mdx'
+import SportsBettingIntro from './sports-betting-site/intro.mdx'
+import SportsBettingPart1 from './sports-betting-site/part-1.mdx'
+import SportsBettingPart2 from './sports-betting-site/part-2.mdx'
+import TerrySSO1 from './terry-sso/terry-sso1.mdx'
+
 import './styles.scss'
 
-export default function BlogPage(): ReactElement {
+export default function BlogPage(props): ReactElement {
+  const location = useLocation()
+  console.log({ props })
+  console.log({ location })
   return (
     <div>
-      Hello, world!
-      <Helloo />
+      <div>
+        <Link to="/articles/sports-betting/intro">Intro</Link>
+        <Link to="/articles/sports-betting/part-1"> Part 1</Link>
+        <Link to="/articles/sports-betting/part-2"> Part 2</Link>
+      </div>
+      <div className={classnames('article')}>
+        <Switch>
+          <Route path="/articles/ineffective-java/set-contains">
+            <IneffJavaHashSet />
+          </Route>
+          <Route path="/articles/sports-betting/intro">
+            <SportsBettingIntro />
+          </Route>
+          <Route path="/articles/sports-betting/part-1">
+            <SportsBettingPart1 />
+          </Route>
+          <Route path="/articles/sports-betting/part-2">
+            <SportsBettingPart2 />
+          </Route>
+          <Route path="/articles/terry-sso/terr-sso1">
+            <TerrySSO1 />
+          </Route>
+        </Switch>
+      </div>
     </div>
   )
 }
